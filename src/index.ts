67b92d729beb86234e0aa7384f50b8e62a3b480e
Norah-bj <inezanorah12@gmail.com>
2025-12-05 14:30:17 +0200

    Remove

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (_req: Request, res: Response) => {
+  res.send("Hello Nora — TS + Node is working!");
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
